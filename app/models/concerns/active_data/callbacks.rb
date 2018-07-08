c8f00d0de8a73fae8970c546005975ac3f542b91
Jonas Hübotter <me@jonhue.me>
2018-07-08 20:25:04 +0200

    [#22] App is not a symbol nor a string

--- app/models/concerns/active_data/callbacks.rb
+++ app/models/concerns/active_data/callbacks.rb
@@ -5,48 +5,52 @@ module ActiveData
     extend ActiveSupport::Concern
 
     included do
-      cattr_accessor :before_save, :after_save,
-        :before_create, :after_create,
-        :before_update, :after_update,
-        :before_destroy, :after_destroy
+      cattr_accessor :before_save_callbacks, :after_save_callbacks,
+        :before_create_callbacks, :after_create_callbacks,
+        :before_update_callbacks, :after_update_callbacks,
+        :before_destroy_callbacks, :after_destroy_callbacks
+
+      include ClassMethods
     end
 
-    def before_save(m)
-      add_callback(@@before_save, m)
-    end
+    module ClassMethods
+      def before_save(m)
+        add_callback(@@before_save_callbacks, m)
+      end
 
-    def after_save(m)
-      add_callback(@@after_save, m)
-    end
+      def after_save(m)
+        add_callback(@@after_save_callbacks, m)
+      end
 
-    def before_create(m)
-      add_callback(@@before_create, m)
-    end
+      def before_create(m)
+        add_callback(@@before_create_callbacks, m)
+      end
 
-    def after_create(m)
-      add_callback(@@after_create, m)
-    end
+      def after_create(m)
+        add_callback(@@after_create_callbacks, m)
+      end
 
-    def before_update(m)
-      add_callback(@@before_update, m)
-    end
+      def before_update(m)
+        add_callback(@@before_update_callbacks, m)
+      end
 
-    def after_update(m)
-      add_callback(@@after_update, m)
-    end
+      def after_update(m)
+        add_callback(@@after_update_callbacks, m)
+      end
 
-    def before_destroy(m)
-      add_callback(@@before_destroy, m)
-    end
+      def before_destroy(m)
+        add_callback(@@before_destroy_callbacks, m)
+      end
 
-    def after_destroy(m)
-      add_callback(@@after_destroy, m)
+      def after_destroy(m)
+        add_callback(@@after_destroy_callbacks, m)
+      end
     end
 
     def run_callbacks(callback, abort_with_false = false)
       valid = true
-      self.class.send(callback)&.each do |m|
-        valid = method(m)
+      self.class.send("#{callback}_callbacks")&.each do |m|
+        valid = send(m)
         break if valid == false && abort_with_false
       end
       valid
