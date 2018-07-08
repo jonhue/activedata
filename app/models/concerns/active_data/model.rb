c95c4bcd6fcd9cf4f70206a1b7c22f49e0bb56ca
Jonas Hübotter <me@jonhue.me>
2018-07-08 22:17:33 +0200

    Revert "[#61] Return an empty array for `self.all` as default" (#65)
    * Revert "[#63] Load data for classes which are not abstract (#64)"
    
    This reverts commit c833788a42d12eb35bde0fbd791f2af011aa1012.
    
    * Revert "[#61] Return an empty array for `self.all` as default (#62)"
    
    This reverts commit 6c5a7441e49178d0e11a1345736ed36e03831414.

--- app/models/concerns/active_data/model.rb
+++ app/models/concerns/active_data/model.rb
@@ -10,8 +10,7 @@ module ActiveData
     include ActiveData::Associations
 
     included do
-      cattr_accessor :abstract_class
-      cattr_writer :all
+      cattr_accessor :all, :abstract_class
       cattr_reader :active_data_config
       attr_accessor :id
 
@@ -55,10 +54,6 @@ module ActiveData
 
       def find(param)
         param.is_a?(Array) ? param.map { |id| find_by(id: id) } : find_by(id: param)
-      end
-
-      def all
-        @@all || []
       end
 
       def dataset
