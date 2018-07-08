263ecbe8fe615fcaa9e1fb507b0c4d7a28cd3166
Jonas Hübotter <me@jonhue.me>
2018-07-08 22:02:51 +0200

    [#57] Recognize abstract classes (#59)

--- lib/active_data/railtie.rb
+++ lib/active_data/railtie.rb
@@ -4,7 +4,7 @@ module ActiveData
   class Railtie < Rails::Railtie
 
     initializer 'activedata.load' do
-      ActiveData::Load.new(ObjectSpace.each_object(Class).select { |c| c.abstract_class && c.included_modules.include?(ActiveData::Model) }).perform
+      ActiveData::Load.new(ObjectSpace.each_object(Class).select { |c| c.try(:abstract_class) && c.included_modules.include?(ActiveData::Model) }).perform
     end
 
   end
