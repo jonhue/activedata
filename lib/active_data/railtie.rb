e9fd6107b934bcc431c70057bd5c070970f9ad50
Jonas Hübotter <me@jonhue.me>
2018-07-09 19:16:06 +0200

    [#92] Run load hooks (#93)

--- lib/active_data/railtie.rb
+++ lib/active_data/railtie.rb
@@ -4,7 +4,7 @@ module ActiveData
   class Railtie < Rails::Railtie
 
     initializer 'activedata.load' do
-      ActiveSupport.on_load :activedata do
+      ActiveSupport.on_load :active_data do
         if defined?(ApplicationData)
           ApplicationData.descendants&.each do |c|
             c.dataset.load unless c.delay_loading?
