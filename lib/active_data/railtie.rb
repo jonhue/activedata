b9f42a3533369499a8f5d4d57306300f027bb6d9
Jonas Hübotter <me@jonhue.me>
2018-07-09 17:56:59 +0200

    [#69] Non-abstract classes inherit abstract_class = true from ApplicationData

--- lib/active_data/railtie.rb
+++ lib/active_data/railtie.rb
@@ -4,7 +4,7 @@ module ActiveData
   class Railtie < Rails::Railtie
 
     initializer 'activedata.load' do
-      ActiveData::Load.new(ObjectSpace.each_object(Class).select { |c| c.included_modules.include?(ActiveData::Model) && !c.abstract_class? }).perform
+      ActiveData::Load.new(ObjectSpace.each_object(Class).select { |c| c.included_modules.include?(ActiveData::Model) && c != ActiveData::Base && c != ApplicationData }).perform
     end
 
   end
